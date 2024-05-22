04cc8ee2c51cd62d0cbe877aed19224bd0a011d1
SohirMaskey <95291761+SohirMaskey@users.noreply.github.com>
2024-05-22 09:08:32 +0200

    Update qm9.sh

--- scripts/qm9.sh
+++ scripts/qm9.sh
@@ -9,5 +9,5 @@ python run_model.py --dataset qm9_6 --r 5 --use_edge_attr --num_edge_encoder_lay
 python run_model.py --dataset qm9_7 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 6 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
 python run_model.py --dataset qm9_8 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 5 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
 python run_model.py --dataset qm9_9 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 6 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
-python run_model.py --dataset qm9_6 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 4 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
-python run_model.py --dataset qm9_6 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 4 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
+python run_model.py --dataset qm9_10 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 4 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
+python run_model.py --dataset qm9_11 --r 5 --use_edge_attr --num_edge_encoder_layers 2 --num_layers 4 --num_reps 4 --norm BatchNorm1d --num_epochs 400 --lr_scheduler_decay_rate 0.9 --lr_scheduler_patience 10
